6fe058c2463ce46f711a0dbea8a3f5982dce679b
Mateo Lopez <mateolr.97@gmail.com>
2025-02-20 20:40:41 -0500

    create repo and branches

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 terraform {
-    required_providers {
-        github = {
-            source = "integrations/github"
-            version = "~> 5.0"
-        }
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 5.0"
     }
+  }
 }
 
 provider "github" {
-    token = var.github_token
-    owner = var.github_org
+  token = var.github_token
+  owner = var.github_org
 }
 
 variable "github_token" {
@@ -24,6 +24,32 @@ variable "github_org" {
 }
 
 resource "github_membership" "membership_for_malopezru" {
-    username = "malopezru"
-    role     = "admin"
+  username = "malopezru"
+  role     = "admin"
+}
+
+resource "github_repository" "terraform-lab-iac" {
+  name        = "terraform-lab-iac"
+  description = "Profe pongame 5"
+  visibility  = "public"
+  template {
+    owner                = "MarketingPipeline"
+    repository           = "Awesome-Repo-Template"
+    include_all_branches = true
+  }
+}
+
+resource "github_branch" "development" {
+    repository = "terraform-lab-iac"
+    branch     = "development"
+}
+
+resource "github_branch" "test" {
+    repository = "terraform-lab-iac"
+    branch     = "test"
+}
+
+resource "github_branch" "staging" {
+    repository = "terraform-lab-iac"
+    branch     = "staging"
 }
